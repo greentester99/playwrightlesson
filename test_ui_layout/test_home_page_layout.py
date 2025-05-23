bbc587dad10e99c4682fbd0819c3902f1999dd80
greenmusic <realjaygreenmusic@gmail.com>
2025-05-23 10:06:12 +0100

    edited test_home_page_layout.py to pass

---
+++ test_ui_layout/test_home_page_layout.py
@@ -0,0 +1,36 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+from pom.home_page_elements import HomePageElements
+import pytest
+
+@pytest.mark.integration
+def test_about_us_section_verbiage(login_set_up)-> None:
+    page=login_set_up
+    # browser = playwright.chromium.launch(headless=False)
+    # page = browser.new_page()
+    # page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    # page.set_default_timeout(2000)
+    home_page = HomePageElements(page)
+    expect(home_page.celebrate_header).to_be_visible()
+    expect(home_page.celebrate_body).to_be_visible()
+
+
+
+    print("About the section verbiage")
+
+
+@pytest.mark.regression
+def test_about_us_section_verbiage_2(login_set_up)->None:
+    page=login_set_up
+    # browser = playwright.chromium.launch(headless=False)
+    # page = browser.new_page()
+    # page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    # page.set_default_timeout(2000)
+    home_page = HomePageElements(page)
+    # expect(page.locator("text=yooo")).to_be_visible()
+    expect(home_page.celebrate_header).to_be_visible()
+    expect(home_page.celebrate_body).to_be_visible()
+
+
+
+    print("About the section verbiage")
+
