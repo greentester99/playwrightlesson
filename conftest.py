763f117008d10bf4f14d2625379f4d6493c91331
greenmusic <realjaygreenmusic@gmail.com>
2025-05-23 10:37:11 +0100

    edited

--- conftest.py
+++ conftest.py
@@ -1,9 +1,14 @@
+import os
+
 import pytest
 import playwright
 from playwright.sync_api import Playwright, expect
+import time
+import pytest
+# import utils.secret_config
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def set_up(browser):
     # browser = playwright.chromium.launch(headless=False,) #slow_mo=500#
     context = browser.new_context()
@@ -17,7 +22,7 @@ def set_up(browser):
     page.close()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def login_set_up(set_up):
     # browser = playwright.chromium.launch(headless=False,) #slow_mo=500#
     # context = browser.new_context()
@@ -35,7 +40,9 @@ def login_set_up(set_up):
     page.click("'Log in with Email' ")
     page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").fill("symon.storozhenko@gmail.com")
     page.get_by_test_id("emailAuth").get_by_role("textbox", name="Email").press("Tab")
-    page.get_by_role("textbox", name="Password").fill("test123", )
+    # page.get_by_role("textbox", name="Password").fill(utils.secret_config.PASSWORD )
+    page.get_by_role("textbox", name="Password").fill(os.environ["PASSWORD"] )
+
     page.get_by_role("textbox", name="Password").press("Enter")
     expect(page.get_by_role("button", name="Log in with Email")).to_be_hidden()
     expect(page.locator("text=Fashion You’ll Love")).to_be_visible()
